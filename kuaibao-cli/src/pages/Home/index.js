73b2cecf5a19c42b16b7e59b1796cb551fd60e68
samhoo <564153675@qq.com>
2019-07-24 15:20:58 +0800

    1111

--- kuaibao-cli/src/pages/Home/index.js
+++ kuaibao-cli/src/pages/Home/index.js
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 class Home extends Component{
     render(){
         return <div>
-            <p style={{height:'500px',background:'red'}}>首页</p>
+            <p style={{height:'500px',background:'red'}}>首页111111</p>
             <p style={{height:'500px',background:'yellow'}}>首页</p>
             <p style={{height:'500px',background:'red'}}>首页</p>
             <p style={{height:'500px',background:'green'}}>首页</p>
